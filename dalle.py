d5ce19c8b81fd85dcec108e30eb7980629c7310e
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-01-26 21:48:21 +0900

    Delete dalle.py

--- dalle.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st  
-
-st.title("Image generator")
-st.write("This is the DALL-E image generator.")
-st.write("Share your pet's unique traits, from behavior to personality. Introducing the DALL-E app that paints a picture of their family.")
